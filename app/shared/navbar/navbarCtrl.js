e63a163c4132dcb50c739971e831c600ab50768c
David Kanfi <davidk1979@gmail.com>
2019-07-25 11:47:13 +0300

    sidebar open  and close + hamburger + x

--- app/shared/navbar/navbarCtrl.js
+++ app/shared/navbar/navbarCtrl.js
@@ -1,3 +1,19 @@
 app.controller("navbarCtrl", function($scope) {
     
+$scope.isOpen = false
+
+$scope.openSide=function(){
+$scope.isOpen=true;
+}
+    
+$scope.closeSide=function(){
+$scope.isOpen=false;
+}
+
+$scope.goToCoursesPage = function() {
+    $rootScope.isOpen = false;
+    $state.transitionTo('courses');
+};
+
+
 })
