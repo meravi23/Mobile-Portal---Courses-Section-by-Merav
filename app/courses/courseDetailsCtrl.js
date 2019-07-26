708ebf5d040520188e5fa0ab5f23d092719161d5
Merav <merav.mi@gmail.com>
2019-07-26 15:15:14 +0300

    getCourse() + arrow img - goBack()

--- app/courses/courseDetailsCtrl.js
+++ app/courses/courseDetailsCtrl.js
@@ -1,5 +1,35 @@
-app.controller("courseDetailsCtrl", function($scope, $location) {
+app.controller("courseDetailsCtrl", function ($scope, $location, server) {
     $scope.courseid = ($location.path()).substr(9);
     console.log("Course ID: " + $scope.courseid);
 
+    $scope.getCourse = function () {
+        var data = {};
+        data.courseid = $scope.courseid;
+        server.requestPhp(data, 'GetCourseById').then(function (data) {
+            $scope.course = data;
+            console.log($scope.course);
+
+            //an array keeping a list of subjects that should be deleted once course id is saved
+            // $scope.course.subjectsToDelete = [];
+            // getProjects();
+            // getCustomFields();
+            // $scope.getEnrolledUsersInRole(1);
+            // $scope.getEnrolledUsersInRole(2)
+        });
+    };
+
+
+    if ($scope.courseid) {
+        $scope.getCourse();
+
+    } else {
+        $scope.course = {};
+        $scope.course.status = 1;
+        $scope.course.subjects = [];
+    }
+
+
+    $scope.goBack = function () {
+        $location.path('/courses');
+    }
 });
