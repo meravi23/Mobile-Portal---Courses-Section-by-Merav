4a5bafd404d7053384aca6685bf26140bbd91f45
Merav <merav.mi@gmail.com>
2019-07-26 14:40:27 +0300

    courses: retreived courseId from $location.path()

--- app/courses/courseDetailsCtrl.js
+++ app/courses/courseDetailsCtrl.js
@@ -1,3 +1,5 @@
-app.controller("courseDetailsCtrl", function($scope) {
-   
+app.controller("courseDetailsCtrl", function($scope, $location) {
+    $scope.courseid = ($location.path()).substr(9);
+    console.log("Course ID: " + $scope.courseid);
+
 });
