e7f4e9729e03f75da0af51f99f9b77de2a50b222
David Kanfi <davidk1979@gmail.com>
2019-07-31 12:46:41 +0300

    User Links

--- app/shared/navbar/navbarCtrl.js
+++ app/shared/navbar/navbarCtrl.js
@@ -1,22 +1,39 @@
-app.controller("navbarCtrl", function($scope) {
+app.controller("navbarCtrl", function ($scope, $location) {
 
 
-    
-$scope.isOpen = false
-$scope.openSide=function(){
-$scope.isOpen=true
-}
-    
-$scope.closeSide=function(){
-$scope.isOpen=false;
-}
 
-// $scope.usermenu=false;
+    $scope.isOpen = false
+    $scope.openSide = function () {
+        $scope.isOpen = true
+    }
 
-$scope.usermenuclick=function(){
-    $scope.usermenu=!$scope.usermenu;
-    
-}
+    $scope.closeSide = function () {
+        $scope.isOpen = false;
+    }
 
+    // $scope.usermenu=false;
+
+    $scope.usermenuclick = function () {
+        $scope.usermenu = !$scope.usermenu;
+
+    }
+
+    // $location.absUrl().includes("staff");
+
+    $scope.menuIndex = function () {
+        if ($location.path() === "/courses") {
+            return "קורסים";
+        } else if ($location.absUrl().includes("staff")) {
+            return "עובדים";
+        } else if ($location.absUrl().includes("student")) {
+            return "חניכים";
+        } else if ($location.absUrl().includes("new")) {
+            return "משתמשים חדשים";
+        } else if ($location.path() === "/hours-report") {
+            return "דיווח שעות";
+        } else if ($location.path() === "/hours-approve") {
+            return "אישור שעות";
+        }
+    }
 
 })
